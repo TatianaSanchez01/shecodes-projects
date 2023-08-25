744e83e89673bbe0158cb19e6e6023674f783ba6
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-08-25 15:59:34 -0500

    [FEATURE] added unit change function

--- script.js
+++ script.js
@@ -7,12 +7,16 @@ let sunset = document.querySelector("#sunset");
 let humidity = document.querySelector("#weather-humidity");
 let wind = document.querySelector("#weather-wind");
 let description = document.querySelector("#weather-description");
-
+let farenheitLink = document.querySelector("#farenheit-link");
+let celsiusLink = document.querySelector("#celsius-link");
 let form = document.querySelector("#search-form");
 
 form.addEventListener("submit", handleSubmit);
-
+farenheitLink.addEventListener("click", displayFarenheitTemp);
+celsiusLink.addEventListener("click", displayCelsiusTemp);
+let ceulsiusTemperature = temp;
 function displayTemperature(response) {
+    ceulsiusTemperature = Math.round(response.data.main.temp);
     temp.innerHTML = Math.round(response.data.main.temp);
     city.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
     description.innerHTML = `${response.data.weather[0].description}`;
@@ -57,5 +61,20 @@ function search(city) {
     let apiKey = "6330484588203ae9bc8288a285d5dc8b";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
     axios.get(apiUrl).then(displayTemperature);
+}
+
+function displayFarenheitTemp(event) {
+    event.preventDefault();
+    let farenheitTemperature = (ceulsiusTemperature * 9) / 5 + 32;
+    temp.innerHTML = Math.round(farenheitTemperature);
+    celsiusLink.classList.remove("active");
+    farenheitLink.classList.add("active");
+}
+
+function displayCelsiusTemp(event) {
+    event.preventDefault();
+    temp.innerHTML = ceulsiusTemperature;
+    celsiusLink.classList.add("active");
+    farenheitLink.classList.remove("active");
 }
 
